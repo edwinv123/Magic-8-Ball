69ad7c49559183bf815bda81733cd6fa8f1b5cad
Edwin <126evalentinnunez@frhsd.com>
2024-11-26 12:54:12 -0500

    changeeeee

---
+++ js/sandbox.js
@@ -0,0 +1,13 @@
+const points = [
+    "Idk",
+    "Maybe",
+    "Sigma",
+    "Yes",
+    "No",
+    "Most likely",
+];
+document.getElementById("response").innerHTML=points;
+
+function myFunction(){
+    points.s
+}
